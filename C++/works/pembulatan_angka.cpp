a585c69f43353e29c73dfe76ee471d17b1dc16d1
afdesem <97596838+afdesem@users.noreply.github.com>
2024-07-25 10:10:46 +0700

    Update pembulatan_angka.cpp

--- C++/works/pembulatan_angka.cpp
+++ C++/works/pembulatan_angka.cpp
@@ -4,25 +4,21 @@
 
 
 int main () {
-    //Declare 'em variables
     float pecahan, vround, vceil, vfloor, vtrunc;
 
-    //Now make sum user inputs
     std::cout << "Masukkan bilangan pecahan: ";
     std::cin >> pecahan;
     
 
-    //now assign more variables and round those shits
     vround = round(pecahan);
     vceil = ceil(pecahan);
     vfloor = floor(pecahan);
     vtrunc = trunc(pecahan);
 
-    //fucking output all those shits
-    std::cout << "Hasil: " << vround << std::endl;
-    std::cout << "Hasil: " << vceil << std::endl;
-    std::cout << "Hasil: " << vfloor << std::endl;
-    std::cout << "Hasil: " << vtrunc << std::endl;
+    std::cout << "Hasil vround: " << vround << std::endl;
+    std::cout << "Hasil vceil: " << vceil << std::endl;
+    std::cout << "Hasil vfloor: " << vfloor << std::endl;
+    std::cout << "Hasil vtrunc: " << vtrunc << std::endl;
     
     return 0;
 }
