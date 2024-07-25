edcb396a4bfd6abd3edb491e9ff963fe3ebb7405
afdesem <97596838+afdesem@users.noreply.github.com>
2024-07-25 08:45:20 +0700

    Create arrray_dalam_perulangan.cpp

---
+++ C++/fixed/arrray_dalam_perulangan.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int i;
+    float bil[7], jumlah, rata2;
+
+    for (int i = 0; i < 7; i++) {
+        do {
+            cout << i + 1 << ". Masukan angka ke-" << i + 1 << ": ";
+            cin >> bil[i];
+            //Prevents and/or stops the code before breaking in a while loop error
+            cin.clear();
+            cin.ignore(1000, '\n');
+            if (bil[i] > 100 || bil[i] <= 0) {
+                cout << "Error! Angka di antara 1 s/d 100" << endl;
+            }
+        } while (bil[i] > 100 || bil[i] <= 0);
+        jumlah += bil[i];
+    }
+
+    rata2 = jumlah / 7;
+    cout << "Rata2 Bilangan di atas = " << rata2 << endl;
+
+    return 0;
+}
