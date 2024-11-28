fdb151a8403898da937a970c36f285609d27da04
afdesem <97596838+afdesem@users.noreply.github.com>
2024-11-29 00:36:47 +0700

    Update number_guessing.cpp

--- C++/others/number_guessing.cpp
+++ C++/others/number_guessing.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <random>
+#include <ctime>
 
+int main(int argc, char ** argv) {
+  srand(time(0));
+  int num = rand() % 100, answer;
+  
+  system("cls");
+  
+  std::cout << "Masukan angka tebakanmu!(1-100): ";
+  std::cin >> answer;
+  
+  while (answer != num) {
+    system("cls");
+    
+    if (answer > num) {
+      std::cout << "Lebih rendah lagi!\n";
+    } else if (answer < num) {
+      std::cout << "Lebih tinggi lagi!\n";
+    }
+    
+    std::cout << "Masukan angka tebakanmu!(1-100): ";
+    std::cin >> answer;
+    
+  }
+  
+  std::cout << "Benar! jawabannya adalah: " << num << std::endl;
+}
